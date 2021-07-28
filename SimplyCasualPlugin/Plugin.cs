584679cee55c6cdaf4584269006b2c4d10b0e0b9
dawnvt <64136899+dawnvt@users.noreply.github.com>
2021-07-28 18:31:40 +0200

    Add project files with some functionality

---
+++ SimplyCasualPlugin/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using IPA;
+using SiraUtil.Zenject;
+using IPALogger = IPA.Logging.Logger;
+using IPA.Config.Stores;
+using SimplyCasualPlugin.Installers;
+using Config = IPA.Config.Config;
+using SimplyCasualPlugin.Configuration;
+
+namespace SimplyCasualPlugin
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        private PluginConfig _config;
+
+        [Init]
+        public void Init(Zenjector zenjector, PluginConfig config)
+        {
+            zenjector.OnMenu<MenuInstaller>().WithParameters(_config);
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+        }
+    }
+}
