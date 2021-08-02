bd0321ef3f2b254ab152f462c799e82495446ca2
dawnvt <64136899+dawnvt@users.noreply.github.com>
2021-08-02 22:48:28 +0200

    Added basic HTTP functionality

--- SimplyCasualPlugin/ScoreUpload/Upload.cs
+++ SimplyCasualPlugin/ScoreUpload/Upload.cs
@@ -1,7 +1,34 @@
-﻿namespace SimplyCasualPlugin.ScoreUpload
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SimplyCasualPlugin.ScoreUpload
 {
     public class Upload
     {
-        
+        public Upload()
+        {
+            
+        }
+
+        static async Task Main()
+        {
+            HttpClient client = new HttpClient();
+            
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                
+                Console.WriteLine(responseBody);
+            }
+            
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
     }
 }
