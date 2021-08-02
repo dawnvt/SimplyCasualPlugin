397d5425ccc78a2b578237e44d6dacca35cada91
dawnvt <64136899+dawnvt@users.noreply.github.com>
2021-08-02 14:40:41 +0200

    Start of the uploading (obfuscating later, you cant get anything useful out of this either way)

---
+++ SimplyCasualPlugin/User/UserDefinition.cs
@@ -0,0 +1,13 @@
+﻿using BS_Utils.Gameplay;
+using JetBrains.Annotations;
+
+namespace SimplyCasualPlugin.User
+{
+    public class UserDefinition
+    {
+        public UserInfo UserProvider()
+        { 
+           return GetUserInfo.GetUserAsync().GetAwaiter().GetResult();
+        }
+    }
+}
