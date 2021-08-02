7a4e2d8bdd112e25100a446050e9f97e6b2dccc0
dawnvt <64136899+dawnvt@users.noreply.github.com>
2021-08-02 15:22:49 +0200

    More UI changes

--- SimplyCasualPlugin/UI/ModSettingsView.cs
+++ SimplyCasualPlugin/UI/ModSettingsView.cs
@@ -1,9 +1,29 @@
-﻿using BeatSaberMarkupLanguage.ViewControllers;
+﻿using System.ComponentModel;
+using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.ViewControllers;
+using IPA.Config.Data;
+using SimplyCasualPlugin.Configuration;
 
 namespace SimplyCasualPlugin.UI
 {
     public class ModSettingsView : BSMLAutomaticViewController
     {
+        private PluginConfig _config;
+        public ModSettingsView(PluginConfig config)
+        {
+            _config = config;
+        }
+        
+        [UIValue("leaderboardToggle")]
+        internal bool EnableLeaderboard
+        {
+            get => _config.isSimplyCasualEnabled;
+            set
+            {
+                _config.isSimplyCasualEnabled = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(EnableLeaderboard)));
+            } 
+        }
         
     }
 }
